1b65fdc1f5e2b91b3f2808c9ac36741cbd6ca696
CO18326 <42867901+CO18326@users.noreply.github.com>
2019-01-23 15:35:09 +0530

    Update assign5.c

--- assign5.c
+++ assign5.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 int main ()
-{printf("This is the pstree.\n");           /* print pstree(a system call) */
+{printf("This is the pstree.\n");           /* print pstree(a system call) 
 system("pstree");
 return 0;
 }
